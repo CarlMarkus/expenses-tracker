e3a35a395ab3beec0964739695b60329b5aa3966
Carl Markus <cmm_92@hotmail.com>
2023-01-05 20:12:13 +0100

    changes to date format

--- src/components/expenses/ExpenseDate.js
+++ src/components/expenses/ExpenseDate.js
@@ -1,14 +1,14 @@
 import "./ExpenseDate.css";
 function ExpenseDate(props) {
-  /*må endre på formatet */ 
+  /*må endre på formatet */
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
     <div className="expense-date">
-      <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
+      <div className="expense-date__month">{month}</div>
       <div className="expense-date__day">{day}</div>
     </div>
   );
